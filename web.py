440d4fb81e4ed4f7c956876bb1eca84acb13aecd
pratoshbharani <pratosh.bharanidharan@gmail.com>
2022-12-18 18:03:11 -0500

    Fixed Bug in the for loop: Unable to add duplicate todo tasks

--- web.py
+++ web.py
@@ -10,19 +10,20 @@ def add_todo():
     functions.write_todos(todos)
 
 
+
 st.title('My Todo App')
 st.subheader('This is my Todo App')
 st.write('This App is to increase your productivity')
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    key_todo = f"{todo}_{str(index)}"
+    checkbox = st.checkbox(todo, key=key_todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[key_todo]
         st.experimental_rerun()
 
 st.text_input(label="", placeholder = "Add new todo...",
               on_change=add_todo, key="new_todo")
-
 
